ebab192caa40c28338eefb8e47f94771a74d11e1
Mohammed-mo1911057 <77966719+Mohammed-mo1911057@users.noreply.github.com>
2022-05-31 19:34:18 +0300

    clean up

--- research-internship-main-2/Code/scripts/home.js
+++ research-internship-main-2/Code/scripts/home.js
@@ -1,16 +1,16 @@
 let position = 1;
+let progressBar = 0;
+let initialBar = 0;
 
 document.addEventListener("DOMContentLoaded", start);
 
 async function start(){
     const next = document.querySelector("#next");
     const previous = document.querySelector("#previous");
-
     next.addEventListener("click", (event) => {
         event.preventDefault();
         nextFunction();
     })
-
     previous.addEventListener("click", (event) => {
         event.preventDefault();
         previousFunction();
@@ -26,9 +26,13 @@ function previousFunction(){
 
 function nextFunction(){
     position = position + 1;
+    initialBar = progressBar;           // 0, 6.25, 12.5, 18.75
+    progressBar = progressBar + 6.25;   // 6.25, 12.5, 18.75, ...
     if(position > 16)
         position = 16;
+
     content();
+    move(initialBar, progressBar)
 }
 
 function content(){
@@ -42,4 +46,28 @@ function content(){
         }
         document.getElementById(i).style.display = displayType;
     }
+}
+
+// var i = 0;
+function move(initialBar, progressBar) {
+
+    var elem = document.getElementById("myBar");
+    var end = document.getElementById("myProgress");
+    var width = 0;
+    var id = setInterval(frame, 10);
+
+    document.getElementById("ratio").innerHTML = "Ratio: " + initialBar + " / " +  progressBar + 'Width' + width
+    function frame() {
+        if (width >= 100) {
+            //   clearInterval(id);
+            //   i = 0;
+        } else {
+            end.style.width = progressBar + '%';
+            width++
+            elem.style.width = width + "%";  // 0, 25, 50, 75, 100
+
+        }
+    }
+    //}
+
 }
