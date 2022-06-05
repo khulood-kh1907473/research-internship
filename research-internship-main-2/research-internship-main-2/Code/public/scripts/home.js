6af64bb2dcce74f79959bf7f96234b8801b0af70
Mohammed-mo1911057 <mo1911057@qu.edu.qa>
2022-06-06 00:35:20 +0300

    home page transitions and visuals

--- research-internship-main-2/research-internship-main-2/Code/public/scripts/home.js
+++ research-internship-main-2/research-internship-main-2/Code/public/scripts/home.js
@@ -1,4 +1,4 @@
-let position = 1;
+let position = 0;
 let toggle = false;
 
 document.addEventListener("DOMContentLoaded", start);
@@ -13,25 +13,26 @@ const actives = document.getElementsByClassName(" active");
 
 
 async function start(){
+    nextFunction();
 
     const next = document.querySelector("#next");
     const previous = document.querySelector("#previous");
-    const item1 = document.querySelector("#i1");
-    const item2 = document.querySelector("#i2");
-    const item3 = document.querySelector("#i3");
-    const item4 = document.querySelector("#i4");
-    const item5 = document.querySelector("#i5");
-    const item6 = document.querySelector("#i6");
-    const item7 = document.querySelector("#i7");
-    const item8 = document.querySelector("#i8");
-    const item9 = document.querySelector("#i9");
-    const item10 = document.querySelector("#i10");
-    const item11 = document.querySelector("#i11");
-    const item12 = document.querySelector("#i12");
-    const item13 = document.querySelector("#i13");
-    const item14 = document.querySelector("#i14");
-    const item15 = document.querySelector("#i15");
-    const item16 = document.querySelector("#i16");
+    // const item1 = document.querySelector("#i1");
+    // const item2 = document.querySelector("#i2");
+    // const item3 = document.querySelector("#i3");
+    // const item4 = document.querySelector("#i4");
+    // const item5 = document.querySelector("#i5");
+    // const item6 = document.querySelector("#i6");
+    // const item7 = document.querySelector("#i7");
+    // const item8 = document.querySelector("#i8");
+    // const item9 = document.querySelector("#i9");
+    // const item10 = document.querySelector("#i10");
+    // const item11 = document.querySelector("#i11");
+    // const item12 = document.querySelector("#i12");
+    // const item13 = document.querySelector("#i13");
+    // const item14 = document.querySelector("#i14");
+    // const item15 = document.querySelector("#i15");
+    // const item16 = document.querySelector("#i16");
 
     next.addEventListener("click", (event) => {
         event.preventDefault();
@@ -44,6 +45,8 @@ async function start(){
          previousFunction();
      });
 
+
+
     let showSolution = document.querySelectorAll(".show-answer-button");
     showSolution.forEach((element) => {
         element.addEventListener("click", (event) => {
@@ -52,78 +55,87 @@ async function start(){
             toggleSolution(toggle, element, location);
         })
     });
+    const items = [];
+    for(let i =0; i<16; i++){
+        let item = document.querySelector(`#i${i+1}`);
+         item.addEventListener("click", (event) =>{
+           event.preventDefault();
+           selectItem(i+1);
+       }) ;
+    }
 
-    item1.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(1);
-    })
-    item2.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(2);
-    })
-    item3.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(3);
-    })
-    item4.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(4);
-    })
-    item5.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(5);
-    })
-    item6.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(6);
-    })
-    item7.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(7);
-    })
-    item8.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(8);
-    })
-    item9.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(9);
-    })
-    item10.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(10);
-    })
-    item11.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(11);
-    })
-    item12.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(12);
-    })
-    item13.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(13);
-    })
-    item14.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(14);
-    })
-    item15.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(15);
-    })
-    item16.addEventListener("click", (event) => {
-        event.preventDefault();
-        selectItem(16);
-    })
+    //
+    // item1.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(1);
+    // })
+    // item2.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(2);
+    // })
+    // item3.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(3);
+    // })
+    // item4.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(4);
+    // })
+    // item5.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(5);
+    // })
+    // item6.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(6);
+    // })
+    // item7.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(7);
+    // })
+    // item8.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(8);
+    // })
+    // item9.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(9);
+    // })
+    // item10.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(10);
+    // })
+    // item11.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(11);
+    // })
+    // item12.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(12);
+    // })
+    // item13.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(13);
+    // })
+    // item14.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(14);
+    // })
+    // item15.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(15);
+    // })
+    // item16.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     selectItem(16);
+    // })
 }
 
  function previousFunction(){
      position = position - 1;
      if(position < 1)
          position = 1;
-     content();
+     content(position, position+1);
  }
 
 function nextFunction(){
@@ -131,43 +143,39 @@ function nextFunction(){
     if(position > 16){
         position = 16;
     }
-    content();
+    content(position, position-1);
 }
 
-function content(){
+function content(actual, previous){
 
-    let displayType;
     for(let i = 1; i<17; i++){
         if ( i < position){
             circles[i].classList.add("active");
         }else{
            // circles[i].classList.remove("active");
         }
+    }
+    const actualContent = document.getElementById(actual);
+    actualContent.style.visibility = "visible";
+    actualContent.style.opacity = "1";
+    actualContent.style.transition = "opacity 1s linear";
+    actualContent.style.height = "auto";
 
-        if(i == position) {
-            displayType = 'block';
-        }
-        else {
-            displayType = 'none';
-        }
-        document.getElementById(i).style.display = displayType;   
-    }    
+    if(previous === 0)
+        return;
+
+    const previousContent = document.getElementById(previous);
+    previousContent.style.visibility = "hidden";
+    previousContent.style.opacity = "0";
+    previousContent.style.height = "0";
+
 }
 
 function selectItem(index){
-    
-    for(let i = 1; i<17; i++){
-        if ( index <= actives.length) {
-            if (i==index){
-                document.getElementById(i).style.display = 'block'; 
-            }else{
-                document.getElementById(i).style.display = 'none';  
-            }
-        }else{
-           //document.getElementById("test").innerHTML = "Not allowed";
-        }
-        
-    }
+    let currentLocation = getPosition();
+    position = index;
+    content(index, currentLocation);
+
 }
 
 async function toggleSolution(toggleInstance, toggleButton, location){
